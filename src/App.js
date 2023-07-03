82014d8bb7dfe4704591ca3d93b1f6396d2f3e08
harshtripathi <tripathiharshmani853@gmail.com>
2023-07-03 17:31:35 +0530

    Things done: 1. Conditional rendering 2. Rendering List of data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { people } from "./data.js";
 
 function App() {
   let name = "Amazing Scientist";
@@ -7,12 +8,15 @@ function App() {
     <div className="App">
       <div className="App-header">
         <section>
-        {name}
-        <Profile />
-        <Profile />
-        <Profile />
-        <Profile />
-        <Demo />
+          {name}
+          <Profile />
+          <Profile />
+          <Profile />
+          <Profile />
+          <PassingProp />
+          <PackingList />
+          <ListItems />
+          <Demo />
         </section>
       </div>
     </div>
@@ -27,7 +31,7 @@ function Demo() {
   );
 }
 
- export function Profile() {
+export function Profile() {
   return (
     <>
       <img src="https://placebeard.it/640x360" />
@@ -40,16 +44,90 @@ const person = {
   age: 23,
   graduate: {
     college: "B N M Institute of Technology",
-    degree: "Btech"
-  } 
-}
+    degree: "Btech",
+  },
+};
 
 export function Details() {
-  return(
+  return (
     <>
-    {person.name}
-    {person.graduate.college}
+      {person.name}
+      {person.graduate.college}
     </>
-  )
+  );
 }
+
+export function PassingProp() {
+  return (
+    <Card>
+      <Avatar
+        size={100}
+        people={{
+          name: "katsuko",
+          imageId: "nvv459gnre",
+        }}
+      />
+    </Card>
+  );
+}
+
+function Avatar({ people, size }) {
+  return (
+    <>
+      <p>People of name {people.name}</p>
+      <li>size = {size}</li>
+    </>
+  );
+}
+
+function Card({ children }) {
+  return <div className="card">{children}</div>;
+}
+
+// * Conditional Rendering
+
+function Item({ isPacked, name }) {
+  return (
+    <li className="item">
+      {name} {isPacked && "tick"}
+    </li>
+  );
+}
+
+function PackingList() {
+  return (
+    <section>
+      <h1>Sally rides on Bike</h1>
+      <ul>
+        <Item isPacked={true} name="Sweet Salty" />
+        <Item isPacked={true} name="Bahama Pants" />
+        <Item isPacked={false} name="Liquid vapour" />
+      </ul>
+    </section>
+  );
+}
+
+// * Conditional rendering finish
+
+// * Rendering List
+
+function ListItems() {
+  const listItem = people.map(person => {
+    <li key={person.id}>
+      <p>
+        {person.name}:{" " + person.profession + " "}
+        known for {person.accomplishment}
+      </p>
+    </li>;
+  });
+  return (
+    <>
+      <h1>Scientist</h1>
+      <ul>{listItem}</ul>
+    </>
+  );
+}
+
+// * Rendering List
+
 export default App;
